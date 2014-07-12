333a2d23df745a2d4dabfc9ca11233bfda0a8f83
Dan Fox <iamdanfox@gmail.com>
2014-07-12 13:57:24 +0100

    Support for multiple polylines

--- src/map.js
+++ src/map.js
@@ -1,6 +1,3 @@
-/**
- * @jsx React.DOM
- */
 "use strict";
 
 var __in_node = (typeof exports !== 'undefined' && this.exports !== exports);
@@ -15,6 +12,7 @@ var Map = React.createClass({
   getInitialState: function() {
     return {
       map : null,
+      lines: {},
       markers : []
     };
   },
@@ -28,9 +26,34 @@ var Map = React.createClass({
       width: 500,
       height: 500,
       points: [],
+      lines:{},
       gmaps_api_key: '',
       gmaps_sensor: false
     }
+  },
+
+  // `lines` :: lineName -> {points,strokeColor, strokeWeight ...}
+  updatePolyLine : function(lines) {
+    var keys = Object.keys(lines)
+    for (var i=0;i<keys.length;i++){
+      var lineName = keys[i]
+      var lineData = lines[lineName]
+
+      // add `path` field to lineData
+      var path = [];
+      lineData.points.forEach( function(p){
+        path.push(new google.maps.LatLng(p.latitude, p.longitude));
+      });
+
+      // update existing, or add new
+      if (! this.state.lines[lineName] ) {
+        this.state.lines[lineName] = new google.maps.Polyline(lineData)
+      }
+      this.state.lines[lineName].setMap(this.state.map)
+      this.state.lines[lineName].setOptions(lineData)
+      this.state.lines[lineName].setPath(path)
+    }
+    // TODO: can this remove paths??
   },
 
   // update geo-encoded markers
@@ -72,7 +95,7 @@ var Map = React.createClass({
     }
 
     return (
-      <div style={style}></div>
+      React.DOM.div({style:style})
     );
   },
 
@@ -85,11 +108,12 @@ var Map = React.createClass({
         center: new google.maps.LatLng( this.props.latitude , this.props.longitude ),
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
-      
+
       var map = new google.maps.Map( this.getDOMNode(), mapOptions);
 
       this.setState( { map : map } );
-      this.updateMarkers(this.props.points);
+      if( this.props.points ) this.updateMarkers(this.props.points);
+      if( this.props.lines ) this.updatePolyLine(this.props.lines);
 
     }).bind(this);
 
@@ -102,6 +126,7 @@ var Map = React.createClass({
   // update markers if needed
   componentWillReceiveProps : function(props) {
     if( props.points ) this.updateMarkers(props.points);
+    if( props.lines ) this.updatePolyLine(props.lines);
   }
 
 });
@@ -109,4 +134,3 @@ var Map = React.createClass({
 if( __in_node ) {
   module.exports = Map;
 }
-
